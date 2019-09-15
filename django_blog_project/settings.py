65a618b31b18e54866f3d92ea7cdcf8095b7c739
Negrisan Mihai <mihainegrisan1@gmail.com>
2019-09-15 15:42:41 +0300

    secret key

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '%a#o9(^u45z!d_^^94jrb#$%1-rc7e8zr+tqkukfo^xp@4(_jv'
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
