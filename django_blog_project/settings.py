cf258ace0289375ae3626791954c87ce923ff95f
Negrisan Mihai <mihainegrisan1@gmail.com>
2019-09-15 16:12:56 +0300

    Settings change

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -165,8 +165,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Configure Django App for Heroku.
-if DEBUG == 'False':
-    import django_heroku
-    django_heroku.settings(locals())
+# if DEBUG == 'False':
+import django_heroku
+django_heroku.settings(locals())
 # This will automatically configure DATABASE_URL, ALLOWED_HOSTS, WhiteNoise (for static assets), Logging, and Heroku CI for your application.
 # Bonus points! If you set the SECRET_KEY environment variable, it will automatically be used in your Django settings, too!
